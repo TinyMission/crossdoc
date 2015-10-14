6e452b3dc36b0060b160a0c7490cd743f4768453
Andy Selvig <ajselvig@gmail.com>
2015-10-14 17:04:22 -0500

    added report test case

--- test/test_render.rb
+++ test/test_render.rb
@@ -5,16 +5,22 @@ class TestRender < Minitest::Test
   def setup
   end
 
-  def test_render_pdf
-    doc = CrossDoc::Document.from_file 'test/data/doc.json'
+  def render_pdf_named(name)
+    doc = CrossDoc::Document.from_file "test/data/#{name}.json"
 
-    File.open('test/output/test.json', 'wt') do |f|
+    File.open("test/output/#{name}.json", 'wt') do |f|
       f.write JSON.pretty_generate(doc.to_raw)
     end
 
     renderer = CrossDoc::PdfRenderer.new doc
     renderer.show_overlays = true
-    renderer.to_pdf 'test/output/test.pdf'
+    renderer.to_pdf "test/output/#{name}.pdf"
+  end
+
+
+  def test_render_pdf
+    render_pdf_named 'doc'
+    render_pdf_named 'report'
   end
 
 end
