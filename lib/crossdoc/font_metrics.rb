3194d2dc339ac77d0afe4806f8863b7d5887255a
Calvin Theis <calvin.theis@terrier.tech>
2025-12-23 08:52:21 -0600

    docs(font_metrics): clarify the behavior of num_lines

--- lib/crossdoc/font_metrics.rb
+++ lib/crossdoc/font_metrics.rb
@@ -2,11 +2,18 @@ require 'prawn'
 
 module CrossDoc
 
-  # utility methods for computing font metrics using Prawn's font system
   class FontMetrics
 
     @dummy_document = Prawn::Document.new
 
+    # Compute the number of lines taken by a text string with the given font
+    # after being wrapped at a given width.
+    #
+    # @param text [String] The text to compute.
+    # @param width [Numeric] The width at which to wrap text.
+    # @param font_size [Numeric] The font size in pixels (not points!)
+    # @param font_name [String] The font family whose metrics to use
+    # @returns [Integer] the number of lines taken by the wrapped text
     def self.num_lines(text, width, font_size, font_name='Helvetica')
       font = @dummy_document.find_font font_name
 
